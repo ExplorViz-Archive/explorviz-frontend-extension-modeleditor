06a08646373e10d35822ee5af3fed7ad2abbb0c1
Tim Hackel <timhackel@gmx.de>
2018-05-07 22:15:11 +0200

    added the function to add systems and nodegroups specifically also nodes worked, but right now I wanted to be able to add nodegroups and nodes to EVERY system and nodegroup respectively (name is the value we are searching for) BUT nodes dont work and nodegroups work but throw errors.

--- addon/routes/modelleditor.js
+++ addon/routes/modelleditor.js
@@ -26,29 +26,77 @@ export default BaseRoute.extend(AuthenticatedRouteMixin, {
 			"name": document.getElementById("nSN").value,
 			"parent": landscape  
 	  	});	
+	  	this.get('controller.model.systems').addObject(system);
 	  	system.save();
 	},
 	
 	newNodegroup(landscape){
 		store: service();
-		console.log(landscape.timestamp);
-		console.log(landscape.systems);
 
-		const nodeGroup = this.get('store').createRecord('nodegroup', {
-			"name": document.getElementById("nNgN"),
-			"parent": landscape.systems[0]
-	  	});
+		const landscapeRecord = this.get('controller.model');
+		
+		let system = [];
 
+		let arrayPointer = 0;
+
+		for(let i = 0; i < landscapeRecord.get('systems').length; i++){
+			if(landscapeRecord.get('systems').objectAt(i).name == document.getElementById("nSN").value){
+				system[arrayPointer] = landscapeRecord.get('systems').objectAt(i);
+				arrayPointer++;
+			}
+		}
+		for(let j = 0; j <= arrayPointer; j++){
+			const nodeGroup = this.get('store').createRecord('nodegroup', {
+				"name": document.getElementById("nNgN").value,
+				"parent": system[j]
+		  	});
+
+			system[j].get('nodegroups').addObject(nodeGroup);
+		}
 	},
 
-	newNode(nodegroup){
+	newNode(landscape){
 		store: service();
 		
-		const node = this.get('store').createRecord('node', {
-			"name": document.getElementById("nNN"),
-			"parent": nodegroup  
-	  	});
+		const landscapeRecord = this.get('controller.model');
+		
+		let system = [];
 
+		let systemArrayPointer = 0;
+
+		for(let i = 0; i < landscapeRecord.get('systems').length; i++){
+			if(landscapeRecord.get('systems').objectAt(i).name == document.getElementById("nSN").value){
+				system[systemArrayPointer] = landscapeRecord.get('systems').objectAt(i);
+				systemArrayPointer++;
+			}
+		}
+		console.log(system);
+		let nodegroup = [];
+		nodegroup[0] = [];
+		let nodegroupArrayPointer = 0;
+		for(let i = 0; i < system.length; i++){
+			console.log("noch alles ok");
+			nodegroupArrayPointer = 0;
+			for(let j = 0; j < system[i].get('nodegroups').length; j++){
+				if(system[i].get('nodegroups').objectAt(j).name == document.getElementById("nNgN").value){
+					nodegroup[i][nodegroupArrayPointer] = system[i].get('nodegroups').objectAt(j);
+					nodegroupArrayPointer++;
+					console.log("nodegroup gesetzt")
+				}		
+			}
+		}
+		console.log(nodegroup);
+
+		for(let i = 0; i < nodegroup.length; i++){
+			for(let j = 0; j < nodegroup[i].length; j++){
+				const node = this.get('store').createRecord('node', {
+					"name": document.getElementById("nNN").value,
+					"parent": nodegroup[i][j]  
+			  	});
+
+			  	nodegroup[i][j].get('nodes').addObject(node);
+			}
+		}
 	},
 
 	
